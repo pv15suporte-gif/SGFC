fab788224936299d50af6b18fc5236ee02887e80
pv15suporte-gif <pv15suporte@gmail.com>
2026-07-21 17:09:42 -0300

    Create version.js

---
+++ version.js
@@ -0,0 +1,5 @@
+{
+  "version": "0.1.2",
+  "download_url": "https://github.com/pv15suporte-gif/SGCNH/releases/download/0.1.2/ControleCNH_Setup_0.1.2.exe",
+  "notes": "Correção da conexão MySQL com suporte a SSL/TLS e caching_sha2_password."
+}
